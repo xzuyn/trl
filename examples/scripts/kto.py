cec8b7114aecfcb03b87bc3e87d973f175b432de
xzuyn <16216325+xzuyn@users.noreply.github.com>
2024-09-28 23:03:30 -0400

    Update kto.py

--- examples/scripts/kto.py
+++ examples/scripts/kto.py
@@ -60,6 +60,54 @@ from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
 
 from trl import KTOConfig, KTOTrainer, ModelConfig, get_peft_config, maybe_unpair_preference_dataset, setup_chat_format
+import transformers.modeling_utils
+
+
+# https://github.com/axolotl-ai-cloud/axolotl/pull/1528
+class UnslothOffloadedGradientCheckpointer(torch.autograd.Function):
+    """
+    Saves VRAM by smartly offloading to RAM.
+    Tiny hit to performance, since we mask the movement via non-blocking calls.
+    """
+
+    @staticmethod
+    @torch.cuda.amp.custom_fwd
+    def forward(ctx, forward_function, hidden_states, *args):
+        saved_hidden_states = hidden_states.to("cpu", non_blocking=True)
+        with torch.no_grad():
+            output = forward_function(hidden_states, *args)
+        ctx.save_for_backward(saved_hidden_states)
+        ctx.forward_function = forward_function
+        ctx.args = args
+        return output
+
+    @staticmethod
+    @torch.cuda.amp.custom_bwd
+    def backward(ctx, dY):
+        (hidden_states,) = ctx.saved_tensors
+        hidden_states = hidden_states.to("cuda", non_blocking=True).detach()
+        hidden_states.requires_grad = True
+        with torch.enable_grad():
+            (output,) = ctx.forward_function(hidden_states, *ctx.args)
+        torch.autograd.backward(output, dY)
+        return (
+            None,
+            hidden_states.grad,
+        ) + (
+            None,
+        ) * len(ctx.args)
+
+
+# https://github.com/axolotl-ai-cloud/axolotl/pull/1528
+def hf_grad_checkpoint_unsloth_wrapper(
+    decoder_layer,
+    *args,
+    use_reentrant=None
+):
+    return UnslothOffloadedGradientCheckpointer.apply(
+        decoder_layer.__self__,
+        *args,
+    )
 
 
 # Define and parse arguments.
@@ -73,6 +121,8 @@ class ScriptArguments:
 
 
 if __name__ == "__main__":
+    transformers.modeling_utils.checkpoint = hf_grad_checkpoint_unsloth_wrapper
+
     parser = HfArgumentParser((ScriptArguments, KTOConfig, ModelConfig))
     script_args, training_args, model_args = parser.parse_args_into_dataclasses()
 
